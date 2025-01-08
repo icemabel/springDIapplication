cd6b7d63b26e44c7faaf9e98a4b6491469a14a1f
icemabel <hande_barkan@hotmail.com>
2025-01-08 09:20:34 +0100

    Initial commit

---
+++ src/test/java/com/hande/springDIapplication/SpringDIapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hande.springDIapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDIapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
